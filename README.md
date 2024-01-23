6fe10e2e3c72d23ff8478b1b56714f098db85b9e
carterwward <Carter.Ward@uvm.edu>
2024-01-23 13:53:39 -0800

    add lastfm-data-collection functionality

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# music-data-collator
-Collates the music data retrieved from each collector and writes to the source of truth DB.
+# Usage
---
+++ tests/lastfm_requests_test.py
@@ -0,0 +1,36 @@
+import os
+import unittest
+from dotenv import load_dotenv
+from src.lastfm_requests import LastFMConnection
+
+
+class LastFMConnectionTests(unittest.TestCase):
+    def setUp(self):
+        load_dotenv()
+        api_key = os.getenv("LASTFM_API_KEY")
+        api_secret = os.getenv("LASTFM_API_SECRET")
+        self.lastfm_connection = LastFMConnection(api_key, api_secret)
+
+    def testLibraryArtists(self):
+        cardoor_libary_artists = self.lastfm_connection._get_user_artists(
+            "Car_door", limit=10
+        )
+        self.assertEqual(len(cardoor_libary_artists["artists"]["artist"]), 10)
+
+    def testGetArtistInfo(self):
+        artist_info = self.lastfm_connection._get_artist_info(artist="j dilla")
+        self.assertEqual(
+            (list(artist_info["artist"].keys())),
+            [
+                "name",
+                "mbid",
+                "url",
+                "image",
+                "streamable",
+                "ontour",
+                "stats",
+                "similar",
+                "tags",
+                "bio",
+            ],
+        )
