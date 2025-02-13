20f30ddaa24ddaa15354218aa037e2e13c0d3e82
rafarrel <ralexfarrell@outlook.com>
2025-02-13 18:16:33 -0500

    Setup test artist for query return

--- crate_digger/main.py
+++ crate_digger/main.py
@@ -2,8 +2,21 @@ if not __name__ == '__main__':
     exit()
 
 from crate_server.apis.default_api_base import BaseDefaultApi
+from crate_server.models.artist         import Artist
 from crate_server.models.artist_query   import ArtistQuery
 
 class QueryApi(BaseDefaultApi):
     def search_artist(self, artist_query : ArtistQuery):
-        return [] # TODO: Do something
+        # TODO: Remove test artist
+        test_artist = Artist(
+            "Test artist",
+            "Test disambiguation",
+            "Test genre",
+            "Test life start",
+            "Test life end",
+            "Test country",
+            "Test state_province",
+            "Test municipality"
+        )
+
+        return [test_artist] 
