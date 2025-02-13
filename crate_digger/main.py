df2b3aaac09eb901fee15a470474cc468e43fbb0
rafarrel <ralexfarrell@outlook.com>
2025-02-13 18:07:41 -0500

    Added QueryApi subclass

--- crate_digger/main.py
+++ crate_digger/main.py
@@ -1,2 +1,9 @@
 if not __name__ == '__main__':
     exit()
+
+from crate_server.apis.default_api_base import BaseDefaultApi
+from crate_server.models.artist_query   import ArtistQuery
+
+class QueryApi(BaseDefaultApi):
+    def search_artist(self, artist_query : ArtistQuery):
+        return [] # TODO: Do something
