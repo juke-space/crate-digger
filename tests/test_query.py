3a7daf0b28342894aa14855fa2f52c49121359c8
rafarrel <ralexfarrell@outlook.com>
2025-02-26 20:38:56 -0500

    Removed unnecessary test fixture for test_client

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,5 +1,4 @@
 from fastapi.testclient import TestClient
-import pytest
 
 from crate_digger.main import app
 from crate_server.models.artist_query import ArtistQuery
@@ -7,11 +6,9 @@ from crate_server.models.artist_query import ArtistQuery
 HOST_URL   = "http://localhost:8000"
 ARTIST_URL = f"{HOST_URL}/artist"
 
-@pytest.fixture
-def test_client():
-    yield TestClient(app)
+test_client = TestClient(app)
     
-def test_artist_response_successful(test_client):
+def test_artist_response_successful():
     query = ArtistQuery(genre="hip-hop", country="USA")
     response = test_client.post(ARTIST_URL, json=query.model_dump())
     assert response.status_code == 200
