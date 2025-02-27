f21b812d7f0748cc103944f37141eb5da757ffb0
rafarrel <ralexfarrell@outlook.com>
2025-02-26 20:35:44 -0500

    Removed unnecessary fixture for artist endpoint url in tests

--- tests/test_query.py
+++ tests/test_query.py
@@ -4,17 +4,14 @@ import pytest
 from crate_digger.main import app
 from crate_server.models.artist_query import ArtistQuery
 
-HOST_URL = "http://localhost:8000"
+HOST_URL   = "http://localhost:8000"
+ARTIST_URL = f"{HOST_URL}/artist"
 
 @pytest.fixture
 def test_client():
     yield TestClient(app)
     
-@pytest.fixture
-def artist_url():
-    yield f"{HOST_URL}/artist"
-    
-def test_artist_response_successful(test_client, artist_url):
+def test_artist_response_successful(test_client):
     query = ArtistQuery(genre="hip-hop", country="USA")
-    response = test_client.post(artist_url, json=query.model_dump())
+    response = test_client.post(ARTIST_URL, json=query.model_dump())
     assert response.status_code == 200
