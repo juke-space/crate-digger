1403d5469f2b3c89c25e3c4945d8c2051a0cc02c
carterwward <Carter.Ward@uvm.edu>
2025-03-02 10:28:24 -0800

    format src and tests, change name of crate_digger directory

---
+++ src/main.py
@@ -0,0 +1,36 @@
+"""
+Server entry point
+"""
+
+import fastapi
+import uvicorn
+
+from crate_server.apis.default_api import router
+from crate_server.apis.default_api_base import BaseDefaultApi
+from crate_server.models.artist import Artist
+from crate_server.models.artist_query import ArtistQuery
+
+app = fastapi.FastAPI()
+app.include_router(router)
+
+
+class API(BaseDefaultApi):
+    def search_artist(self, artist_query: ArtistQuery):
+        # TODO: Remove test artist
+        test_artist = Artist(
+            name="Test artist",
+            disambiguation="Test disambiguation",
+            genre="Test genre",
+            life_start="Test life start",
+            life_end="Test life end",
+            country="Test country",
+            state_province="Test state_province",
+            municipality="Test municipality",
+        )
+
+        return [test_artist]
+
+
+if __name__ == "__main__":
+    # TODO: Allow configuration through commandline arguments
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
--- tests/test_query.py
+++ tests/test_query.py
@@ -1,13 +1,14 @@
 from fastapi.testclient import TestClient
 
-from crate_digger.main import app
+from main import app
 from crate_server.models.artist_query import ArtistQuery
 
-HOST_URL   = "http://localhost:8000"
+HOST_URL = "http://localhost:8000"
 ARTIST_URL = f"{HOST_URL}/artist"
 
 test_client = TestClient(app)
-    
+
+
 def test_artist_response_successful():
     query = ArtistQuery(genre="hip-hop", country="USA")
     response = test_client.post(ARTIST_URL, json=query.model_dump())
